379a176025973872656a28dfec2e9af883306f3b
Nikodem Gorak <nikos83@gmail.com>
2019-03-22 13:34:59 +0100

    Connect api and implement simple search

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Search from './views/Search.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -10,14 +11,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      name: 'Search',
+      component: Search
     },
     {
       path: '/about',
       name: 'About',
-
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: About
     }
   ]
 })
